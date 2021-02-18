21dea234fc980a4f2e96306d6d45a50350e4f95a
IchiroT <ichir0roie@gmail.com>
2021-02-18 20:45:07 +0900

    function adjustment of market create

---
+++ RandomWorkMarket.py
@@ -0,0 +1,108 @@
+import random
+
+import pandas as pd
+
+import mplfinance as mpl
+
+import datetime
+
+
+class RandomWorkMarket:
+    def __init__(self):
+        print("rw init")
+
+        self.values = []
+
+        self.candleIndex = []
+        self.candle = []
+
+        self.values.append(100.0)
+
+        self.range = 60 * 5
+
+        self.baseDate = datetime.datetime.now()
+
+        for i in range(self.range * 100):
+            print("set up : " + str(int(i / self.range)) + "%")
+            self.onTick()
+
+    def getVec(self):
+        r = random.random()
+        r -= 0.5
+        r *= 2
+
+        b = random.random() - 0.5
+        b *= 1
+        v = self.values[-1] + r * b
+
+        self.values.append(v)
+        return v
+
+    def onTick(self):
+        # print(len(rw.values))
+        v = self.getVec()
+
+        if len(self.values) % self.range == 0:
+            self.createCandle()
+        return v
+
+    def onCandle(self):
+        for i in range(self.range):
+            self.onTick()
+
+    # what do this.
+    def createCandle(self):  # サイズがちょっとずれてるバグ、影響軽微のだめ無視
+        if len(self.candleIndex) == 0:
+            self.candleIndex.append(self.baseDate)
+        else:
+            nd = datetime.timedelta(seconds=self.range)
+            self.candleIndex.append(self.candleIndex[-1] + nd)
+        buf = [self.values[-self.range]]
+        sl = sorted(self.values[-self.range:-1])
+        buf.append(sl[0])
+        buf.append(sl[-1])
+        buf.append(self.values[-1])
+        self.candle.append(buf)
+
+    def view(self):
+        print(self.values[-1])
+
+    def getCandleData(self, length):
+        df = pd.DataFrame(self.candle[-length:], index=self.candleIndex[-length:],
+                          columns=("Open", "High", "Low", "Close"))
+        return df
+
+    def candleSave(self,length,name):
+        df=self.getCandleData(length)
+        mpl.plot(df,type="candle",savefig=name)
+
+    def candleView(self, length):
+        df = self.getCandleData(length)
+        # print(df)
+        mpl.plot(df, type="candle")
+        # time.sleep(1)
+
+    def genCounts(self, array):
+        return [i for i in range(len(array))]
+
+def someChartCreateAndSave(times):
+    for i in range(times):
+        market = RandomWorkMarket()
+        for j in range(random.randint(100,10000)):
+            market.onTick()
+        name="CandlePng/"+datetime.datetime.now().strftime("candle_%Y%m%d_%H%M%S_")+"_"+str(i)+".pdf"
+        market.candleSave(200,name)
+
+
+
+if __name__ == "__main__":
+    # market = RandomWorkMarket()
+    # for i in range(10000):
+    #     market.onTick()
+
+    # plot.plot(market.genCounts(market.values), market.values)
+    # plot.show()
+
+    # market.candleView(200)
+
+    someChartCreateAndSave(5)
--- iMa.py
+++
@@ -1,9 +0,0 @@
-def simple(values):
-    vals = [(v[0] + v[1] + v[2] + v[3]) / 4 for v in values]
-    ret = 0
-    for i in vals:
-        ret += i
-    return ret / len(values)
-
-
-
